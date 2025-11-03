36895879404819fd2f50ed0e0b784bad3be7323c
Serhii Molozhenko <molozhenko2011@gmail.com>
2025-11-03 11:36:42 +0200

    update form

--- frontend/src/app/Components/comment-form/comment-form.ts
+++ frontend/src/app/Components/comment-form/comment-form.ts
@@ -1,5 +1,4 @@
-// src/app/components/comment-form/comment-form.component.ts
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, input, OnInit, Output, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommentsService } from '../../Services/comments-service';
 import { CommonModule } from '@angular/common';
@@ -10,27 +9,59 @@ import { CommonModule } from '@angular/common';
   imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './comment-form.html'
 })
-export class CommentFormComponent implements OnInit{
+export class CommentFormComponent implements OnInit {
+  @Input() parentCommentId?: string;
   @Output() commentAdded = new EventEmitter<void>();
   selectedFile?: File;
   form!: FormGroup;
 
-  
+  @ViewChild('contentArea') contentArea!: ElementRef<HTMLTextAreaElement>;
 
-  
-  constructor(private fb: FormBuilder, private service: CommentsService) {}
+
+  constructor(private fb: FormBuilder, private service: CommentsService) { }
   ngOnInit(): void {
     this.form = this.fb.group({
-    username: ['', [Validators.required, Validators.pattern(/^[A-Za-z0-9]+$/)]],
-    email: ['', [Validators.required, Validators.email]],
-    homepage: [''],
-    content: ['', Validators.required],
-    parentCommentId: [null]
+      username: ['', [Validators.required, Validators.pattern(/^[A-Za-z0-9]+$/)]],
+      email: ['', [Validators.required, Validators.email]],
+      homepage: [''],
+      content: ['', Validators.required],
+      parentCommentId: [this.parentCommentId ?? null]
     });
   }
 
   onFileSelected(event: any) {
     this.selectedFile = event.target.files[0];
+  }
+  applyItalics(): void {
+    const textarea = this.contentArea.nativeElement;
+    const start = textarea.selectionStart;
+    const end = textarea.selectionEnd;
+    const currentContent = textarea.value;
+
+    if (start < end) {
+      const selectedText = currentContent.substring(start, end);
+      if (currentContent.substring(start - 3, start) === '<i>' && currentContent.substring(end, end + 4) === '</i>') {
+
+        const newContent =
+          currentContent.substring(0, start - 3) +
+          selectedText +
+          currentContent.substring(end + 4);
+
+        this.form.get('content')!.setValue(newContent);
+      } else {
+        const newContent =
+          currentContent.substring(0, start) +
+          `<i>${selectedText}</i>` +
+          currentContent.substring(end);
+
+        this.form.get('content')!.setValue(newContent);
+      }
+      setTimeout(() => {
+        textarea.focus();
+        textarea.selectionStart = start + 3;
+        textarea.selectionEnd = end + 3;
+      }, 0);
+    }
   }
 
   submit() {
