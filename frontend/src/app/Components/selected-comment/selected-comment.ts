36e7978b89edb7b4fcf5b481bd492744a5ad737e
Serhii Molozhenko <molozhenko2011@gmail.com>
2025-11-04 12:43:05 +0200

    update selected comment oninit

--- frontend/src/app/Components/selected-comment/selected-comment.ts
+++ frontend/src/app/Components/selected-comment/selected-comment.ts
@@ -5,7 +5,7 @@ import { CommonModule, DatePipe, Location } from '@angular/common';
 import { ReplyCommentComponent } from '../reply-comment/reply-comment';
 import { CommentFormComponent } from '../comment-form/comment-form';
 import { SelectedComment } from '../../Models/SelectedComment';
-import { Observable } from 'rxjs';
+import { merge, Observable, startWith, Subject, switchMap, tap } from 'rxjs';
 import { Comment } from '../../Models/Comment'
 
 @Component({
@@ -15,26 +15,32 @@ import { Comment } from '../../Models/Comment'
   imports: [CommonModule, DatePipe, CommentFormComponent, ReplyCommentComponent]
 })
 export class SelectedCommentComponent implements OnInit {
-  comment$?: Observable<SelectedComment>;
+  comment$!: Observable<SelectedComment>;
   private id!: string;
+  private reloadTrigger = new Subject<void>();
 
   constructor(private route: ActivatedRoute, private service: CommentsService,private location: Location) { }
 
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      this.id = params.get('id') ?? 'error';
-      this.loadComment();
-    });
+    const idStream = this.route.paramMap.pipe(
+      tap(params => this.id = params.get('id') ?? 'error')
+    );
+    const triggerStream = merge(
+      idStream,
+      this.reloadTrigger.asObservable()
+    ).pipe(
+      startWith(null)
+    );
+    this.comment$ = triggerStream.pipe(
+      switchMap(() => this.service.getCommentById(this.id))
+    );
   }
 
-  loadComment() {
-    this.comment$ = this.service.getCommentById(this.id);
-  }
   download(comment: Comment) {
     this.service.download(comment.id);
   }
   onReplyAdded() {
-    this.loadComment();
+    this.reloadTrigger.next();
   }
   back() {
     this.location.back();
